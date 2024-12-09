88213c53fc0efebf5f4819a26dae489488530ad1
Jayant D. Patil <jayant.ptl@gmail.com>
2024-12-09 19:49:31 +0530

    Initial commit with HelloRedux Project

---
+++ HelloRedux/HelloRedux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloRedux
+//
+//  Created by Jayant D Patil on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloRedux/HelloRedux/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  HelloRedux
+//
+//  Created by Jayant D Patil on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
